0b908536a69942ae4a7b96ca51c905cd7f73377c
nrstirzaker <nrstirzaker.techblog@gmail.com>
2025-06-17 20:11:41 +0100

    add parser and printout body

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.post('/Organisation',(req,res) =>{
-    console.log(req.params);
+    console.log(req.body);
     res.sendStatus(200)
 })
 
