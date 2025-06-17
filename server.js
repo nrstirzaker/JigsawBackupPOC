f2e32fc535b50e691010db8e9a38d387e0f3c545
nrstirzaker <nrstirzaker.techblog@gmail.com>
2025-06-17 16:21:33 +0100

    respond status 200

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ app.get('/', (req, res) => {
 
 app.post('/Organisation',(req,res) =>{
     console.log(req);
+    res.sendStatus(200)
 })
 
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+
 })
